34ac2db356bdd9f1bd3d4bf73b5af37ff068e498
IgorSzyporyn <igor.szyporyn@gmail.com>
2020-08-21 14:07:57 +0200

    fix: was set to develop

--- src/constants.ts
+++ src/constants.ts
@@ -2,4 +2,4 @@ const { name } = require('../package.json')
 
 export const PROJECT_ID = name
 export const CONFIG_FILE_NAME = '.plop-scaffold.json'
-export const IS_DEV = true
+export const IS_DEV = false
